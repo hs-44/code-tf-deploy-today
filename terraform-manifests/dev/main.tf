94c2931374e33c58a2733e0e9681c6336ae39f6d
hs-44 <harshvardhansutar4444@gmail.com>
2025-01-30 15:44:30 +0530

    Update main.tf
    change region name and key pair

--- terraform-manifests/dev/main.tf
+++ terraform-manifests/dev/main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-west-1"
+    region = "ap-south-1"
 }
 
 module "infra_services" {
@@ -23,5 +23,5 @@ module "infra_services" {
   private_cidr = "172.31.2.0/24"
   bucket_name = "testing-terraform-s3-bucket-data"
   bucket_acl  = "private"
-  instance_key_name = "test-tf-key"
+  instance_key_name = "nk1"
 }
